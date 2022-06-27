238670f71c28791fba4207a0ab032f7e454642b5
Michel <michelssalves@gmail.com>
2022-06-27 17:44:16 -0300

    202-06-27

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -483,6 +483,7 @@ class ComposerStaticInita063533aba07ed41d0de282c2db196f0
         'App\\Http\\Middleware\\TrimStrings' => __DIR__ . '/../..' . '/app/Http/Middleware/TrimStrings.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Provider' => __DIR__ . '/../..' . '/app/Provider.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
