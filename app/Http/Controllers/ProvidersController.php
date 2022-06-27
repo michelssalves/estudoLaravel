100b27d57a778ace3fbaf1b67f07a78ba6288d05
unknown <michelssalves@gmail.com>
2022-06-27 09:04:15 -0300

    2022-06-27

--- app/Http/Controllers/ProvidersController.php
+++ app/Http/Controllers/ProvidersController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class ProvidersController extends Controller
 {
     public function index(){
-        return view('app.providers.index');
+        $providers  = [
+            0 => ['name' => 'Provider1', 'status' => 'N', 'cnpj' => '0', 'ddd' => '11', 'phone' => '0000-0000'],
+            1 => ['name' => 'Provider2', 'status' => 'S', 'cnpj' => '0', 'ddd' => '41', 'phone' => '0000-0000'],
+            2 => ['name' => 'Provider3', 'status' => 'S', 'cnpj' => '0', 'ddd' => '55', 'phone' => '0000-0000'],
+        ];
+
+        echo isset($providers[0]['cnpj']) ? 'CNPJ informed ' : 'CNPJ not informed';
+        $providers  = [];
+        return view('app.providers.index', compact('providers'));
     }
 }
