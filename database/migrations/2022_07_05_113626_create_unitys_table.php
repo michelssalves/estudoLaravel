7113c3a6c9cf6537eb242c7c170aea20b011a066
Michel <michelssalves@gmail.com>
2022-07-05 10:43:10 -0300

    2022-07-05 10:43

---
+++ database/migrations/2022_07_05_113626_create_unitys_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unitys', function (Blueprint $table) {
+            $table->id();
+            $table->string('unity', 5);
+            $table->string('description', 30);
+            $table->id();
+            $table->timestamps();
+        });
+
+            Schema::table('products', function(Blueprint $table){
+                $table->unsignedBigInteger('unity_id');
+                $table->foreign('unity_id')->references('id')->on('unitys');
+        });
+
+            Schema::table('products_details', function(Blueprint $table){
+                $table->unsignedBigInteger('unity_id');
+                $table->foreign('unity_id')->references('id')->on('unitys');
+        });
+      
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(){
+
+        Schema::table('products_details', function(Blueprint $table){
+
+            $table->dropForeign('products_details_unity_id_foreign');
+
+            $table->dropColumn('unity_id');
+    
+    });
+
+    Schema::table('products', function(Blueprint $table){
+            
+        $table->dropForeign('products_unity_id_foreign');
+
+        $table->dropColumn('unity_id');
+
+});
+
+        Schema::dropIfExists('unitys');
+    }
+}
